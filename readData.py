1b3d91da811da906bebc6a2efdac1ce2492b93f4
BernhardKonrad <bernhard.konrad@gmail.com>
2014-08-22 14:54:26 +0200

    Reads one data file and plots oxygen vs depth

---
+++ readData.py
@@ -0,0 +1,39 @@
+# In this file we will read the data and plot
+# oxygen level vs depth
+
+f_input = open('Data/met_090_1_001.ctd',
+ 'r')
+
+doneWithHeader = False
+depthList = []
+oxList = []
+
+for line in f_input:
+    if doneWithHeader:
+        # read and save the data
+        dataList = line.split()
+        depth = -float(dataList[1])
+        ox = float(dataList[5])
+        depthList.append(depth)
+        oxList.append(ox)
+
+    else:
+        if "Profile" in line:
+            profile = line.strip().split("=")[1]
+            profile = int(profile)
+        elif "Latitude" in line: 
+            lat = line.strip().split("=")[1]
+            lat = float(lat)
+        elif "Longitude" in line: 
+            lng = line.strip().split("=")[1]
+            lng = float(lng)
+        elif "Columns" in line:
+            # order of columns is consistent
+            doneWithHeader = True
+
+f_input.close()    
+
+import matplotlib.pyplot as plt
+plt.plot(oxList, depthList)
+plt.title('oxygen level at different depths')
+plt.show()
